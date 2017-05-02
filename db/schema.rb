78b61b428e2fd7a69e0db02393843922d8c02222
rezaruslan94 <reza.ruslan94@gmail.com>
2017-05-02 14:18:49 +0700

    add navbar and scaffold department

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502044817) do
+ActiveRecord::Schema.define(version: 20170502065629) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
