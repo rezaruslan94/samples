98974f7e1483dd7d9f387882181effdca569333b
rezaruslan94 <reza.ruslan94@gmail.com>
2017-05-04 23:43:17 +0700

    create new user with admin but not yet fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :employees
   root 'pages#home'
   resources :users
+  namespace :admin do
+  resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502085055) do
+ActiveRecord::Schema.define(version: 20170503083053) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20170502085055) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roles_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "role_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
